e38fea88917f63068c987582af88fa8cb67571da
lloon <70345198+lloon@users.noreply.github.com>
2020-08-27 23:13:50 +0300

    Added AngleSharp. Created parser

---
+++ Kastrulia/source/core/parser/HtmlLoader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kastrulia.source.core.parser
+{
+	class HtmlLoader
+	{
+		readonly HttpClient client;
+		readonly string url;
+
+		public HtmlLoader(IParserSettings settings)
+		{
+			client = new HttpClient();
+			url = $"{settings.BaseUrl}/{settings.Prefix}/";
+		}
+
+		public async Task<string> GetSourceByPageId()
+		{
+			var response = await client.GetAsync(url);
+			string source = null;
+
+			if (response != null && response.StatusCode == HttpStatusCode.OK)
+			{
+				source = await response.Content.ReadAsStringAsync();
+			}
+
+			return source;
+		}
+	}
+}
---
+++ Kastrulia/source/core/parser/ParserWorker.cs
@@ -0,0 +1,106 @@
+﻿using AngleSharp.Html.Parser;
+using Kastrulia.source.core.parser.worms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kastrulia.source.core.parser
+{
+	class ParserWorker<T> where T : class
+	{
+		IParser<T> parser;
+		IParserSettings parserSettings;
+
+		HtmlLoader loader;
+
+		bool isActive;
+		public bool IsNetworkError = false;
+		private ExampleWorm fuelParser;
+
+		#region Properties
+
+		public IParser<T> Parser {
+			get {
+				return parser;
+			}
+			set {
+				parser = value;
+			}
+		}
+
+		public IParserSettings Settings {
+			get {
+				return parserSettings;
+			}
+			set {
+				parserSettings = value;
+				loader = new HtmlLoader(value);
+			}
+		}
+
+		public bool IsActive {
+			get {
+				return isActive;
+			}
+		}
+
+		#endregion
+
+		public event Action<object, T> OnNewData;
+		public event Action<object> OnCompleted;
+
+		public ParserWorker(IParser<T> parser)
+		{
+			this.parser = parser;
+		}
+
+		public ParserWorker(IParser<T> parser, IParserSettings parserSettings) : this(parser)
+		{
+			this.parserSettings = parserSettings;
+		}
+
+		public void Start()
+		{
+			isActive = true;
+			Worker();
+		}
+
+		public void Abort()
+		{
+			isActive = false;
+		}
+
+		private async void Worker()
+		{
+			if (!isActive)
+			{
+				OnCompleted?.Invoke(this);
+				return;
+			}
+
+			try
+			{
+				var source = await loader.GetSourceByPageId();
+				var domParser = new HtmlParser();
+				var document = await domParser.ParseDocumentAsync(source);
+				var result = parser.Parse(document);
+
+				OnNewData?.Invoke(this, result);
+
+				IsNetworkError = false;
+			}
+			catch
+			{
+				IsNetworkError = true;
+			}
+
+			OnCompleted?.Invoke(this);
+
+			isActive = false;
+		}
+
+
+	}
+}
